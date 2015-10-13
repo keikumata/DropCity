981d57362b06a4a0fd2e708aa4f6d434cf862553
JaeHunRo <jhr24@duke.edu>
2015-10-13 18:41:58 +0100

    adds gesture handlers for drop circle

--- Drop/DropCircle.swift
+++ Drop/DropCircle.swift
@@ -2,20 +2,34 @@
 //  DropCircle.swift
 //  Drop
 //
-//  Created by Hun Ro on 10/12/15.
-//  Copyright (c) 2015 Drop. All rights reserved.
+//  Created by Hun Ro on 10/11/15.
+//  Copyright (c) 2015 Hun Ro. All rights reserved.
 //
 
 import UIKit
 
 class DropCircle: UIView {
-
+  
+  // default settings for programmatic
+  let defaultWidth: CGFloat = 20
+  let defaultHeight: CGFloat = 20
+  
+  // customizable
+  var averageDecibel: Float = 0.0
+  var factor: Int = 1
+  
+  // initial position
   var initialX = CGFloat()
   var initialY = CGFloat()
   
-  var panGesture: UIPanGestureRecognizer = UIPanGestureRecognizer()
+  var onPan: ()->() = {}
+  var onRelease: ()->() = {}
+  var onTap: ()->() = {}
   
-  var circleLayer = CAShapeLayer()
+  let panGesture: UIPanGestureRecognizer = UIPanGestureRecognizer()
+  let buttonTap: UIButton = UIButton()
+  
+  let circleLayer = CAShapeLayer()
   var circlePathSmall: UIBezierPath = UIBezierPath()
   
   override init(frame: CGRect) {
@@ -31,23 +45,43 @@ class DropCircle: UIView {
   func setup(frame: CGRect) {
     self.initialX = frame.origin.x
     self.initialY = frame.origin.y
+    
     circlePathSmall = UIBezierPath(ovalInRect: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
     self.backgroundColor = UIColor.clearColor()
     
-    // pan
     self.addGestureRecognizer(panGesture)
     panGesture.addTarget(self, action: "pan:")
-    self.userInteractionEnabled = true
     
+    buttonTap.frame = CGRect(x:0, y:0, width: frame.width, height: frame.height)
+    buttonTap.addTarget(self, action: "tap:", forControlEvents: UIControlEvents.TouchDown)
+    buttonTap.addTarget(self, action: "release:", forControlEvents: UIControlEvents.TouchUpInside)
+    self.addSubview(buttonTap)
+    
+    self.userInteractionEnabled = true
     self.drawCircle(frame)
   }
   
+  // update called from controller
   func updateDecibel(radius: CGFloat) {
     self.animateCircle(radius)
   }
   
+  // gesture handlers
   func pan(sender: UIPanGestureRecognizer) {
-    if (sender.state == UIGestureRecognizerState.Ended) {
+    switch sender.state {
+    case .Changed:
+      var translation: CGPoint = sender.translationInView(self)
+      var newFrame = self.frame
+      newFrame.origin.x += translation.x
+      newFrame.origin.y += translation.y
+      self.frame = newFrame
+      
+      // reset the pangesture to prevent translation from compounding and flying off
+      sender.setTranslation(CGPointMake(0,0), inView: self)
+      circleLayer.opacity = 1.0
+      
+      self.onPan()
+    case .Ended:
       var circleContainerFrame: CGRect = frame
       circleContainerFrame.origin.x = self.initialX
       circleContainerFrame.origin.y = self.initialY
@@ -59,34 +93,30 @@ class DropCircle: UIView {
       frame = circleContainerFrame
       UIView.commitAnimations()
       circleLayer.opacity = 0.6
-    } else {
-      // get new coordinates of pangesture in relation to view (self)
-      var translation: CGPoint = sender.translationInView(self)
       
-      // add translation to view center to reset the center of pangesture (movement)
-      //      sender.view!.center = CGPointMake(sender.view!.center.x + translation.x, sender.view!.center.y + translation.y)
-      var newFrame = self.frame
-      newFrame.origin.x += translation.x
-      newFrame.origin.y += translation.y
-      self.frame = newFrame
-      // reset the pangesture to prevent translation from compounding and flying off
-      sender.setTranslation(CGPointMake(0,0), inView: self)
-      circleLayer.opacity = 1.0
+      self.onRelease()
+    default:
+      break
     }
   }
-
   
+  func tap (sender: UIButton) {
+    circleLayer.opacity = 1.0
+    self.onTap()
+  }
   
-  // draw initial circle
+  func release (sender: UIButton) {
+    circleLayer.opacity = 0.6
+  }
+  
+  // drawing and animation
   func drawCircle(frame: CGRect) {
-    //    circleLayer.frame = frame
     circleLayer.path = circlePathSmall.CGPath
     circleLayer.fillColor = UIColor.whiteColor().CGColor
     circleLayer.opacity = 0.6
     self.layer.addSublayer(circleLayer)
   }
   
-  // animate pulse
   func animateCircle(radius: CGFloat) {
     var circlePathLarge: UIBezierPath {
       return UIBezierPath(ovalInRect: CGRect(x: 0, y: 0, width: (frame.width*radius), height: (frame.height*radius)))
@@ -109,5 +139,5 @@ class DropCircle: UIView {
     animationGroup.repeatCount = 1
     circleLayer.addAnimation(animationGroup, forKey: nil)
   }
-
+  
 }
